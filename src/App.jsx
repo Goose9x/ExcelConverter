edc53725c70b5dc73bb09edd1cfce89f65261a64
Goose9x <kimmaohp99@gmail.com>
2025-04-02 21:57:04 +0700

    Add woo

--- src/App.jsx
+++ src/App.jsx
@@ -1,271 +1,37 @@
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import moment from "moment";
+import ShopifyConverter from "./components/shopifyConveter";
+import WooConverter from "./components/wooConverter";
 import "./App.css";
 
 export default function App() {
   const [convertedData, setConvertedData] = useState([]);
-  const [excelName, setExcelName] = useState("converted");
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    setExcelName(file.name.replace(/\.(csv|xlsx)$/, ""));
-    const reader = new FileReader();
-    setConvertedData([]);
-    reader.onload = (evt) => {
-      const data = new Uint8Array(evt.target.result);
-      const workbook = XLSX.read(data, { type: "array" });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+  const [shopifyName, setShopifyName] = useState("");
+  const [wooName, setWooName] = useState("");
 
-      let previousData;
-      let indexNo = 0;
-      // Chuyển đổi dữ liệu
-      const transformed = jsonData
-        .map((row) => {
-          indexNo = indexNo + 1;
-          if (row["Paid at"] && row["Billing Name"] && row["Billing City"]) {
-            previousData = row;
-            indexNo = 0;
-          }
-          if (
-            row["Lineitem name"]
-              .toLowerCase()
-              .startsWith("add on".toLowerCase()) ||
-            row["Lineitem name"]
-              .toLowerCase()
-              .startsWith("tip".toLowerCase()) ||
-            row["Lineitem name"]
-              .toLowerCase()
-              .startsWith("Shipping Fee".toLowerCase()) ||
-            row["Lineitem name"]
-              .toLowerCase()
-              .startsWith("Resend Fee".toLowerCase())
-          ) {
-            indexNo = indexNo - 1;
-            return {}; // Trả về đối tượng rỗng nếu bắt đầu bằng "add on" hoặc "tip"
-          }
-          var noteResult = handleProcessNotes(previousData);
-          return {
-            "Created at": getCreatedAt(row, previousData),
-            Name: row["Name"],
-            "Lineitem name": row["Lineitem name"],
-            "Lineitem quantity": row["Lineitem quantity"],
-            "Lineitem sku": getLineitemSku(row, previousData),
-            "Shipping Name": getShippingName(row, previousData),
-            "Shipping Street": getShippingStreet(row, previousData),
-            "Shipping Address2": "", // Trống
-            "Shipping City": getShippingCity(row, previousData),
-            "Shipping Zip": getShippingZip(row, previousData),
-            "Shipping Province": getShippingProvince(row, previousData),
-            "Shipping Country": getShippingCountry(row, previousData),
-            "Shipping Country Code": "", // Trống
-            "Shipping Phone": getShippingPhone(row, previousData),
-            Email: getEmail(row),
-            "Customer Notes": "", // Trống
-            Personalization: noteResult[indexNo]?.Personal,
-            Type: noteResult[indexNo]?.Type || noteResult[indexNo]?.Style,
-            Size:
-              noteResult[indexNo]?.Size ||
-              noteResult[indexNo]?.["Men Size"] ||
-              noteResult[indexNo]?.["Women Size"] ||
-              noteResult[indexNo]?.["Kid Size"],
-            Notes: row["Notes"],
-          };
-        })
-        .filter((row) => Object.keys(row).length > 0); // Remove {}
-
-      setConvertedData(transformed);
-    };
-
-    reader.readAsArrayBuffer(file);
+  //handle data
+  const handleDataFromShopify = (dataFromA) => {
+    setConvertedData((prev) => [...prev, ...dataFromA]);
   };
-  //process data
-  const getCreatedAt = (row, previousData) => {
-    return row["Paid at"]
-      ? convertTime(row["Paid at"])
-      : row["Name"] === previousData["Name"]
-      ? convertTime(previousData["Paid at"])
-      : "";
-  };
-  const getLineitemSku = (row, previousData) => {
-    return row["Lineitem sku"]
-      ? row["Lineitem sku"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Lineitem sku"]
-      : "";
-  };
-  const getShippingName = (row, previousData) => {
-    return row["Shipping Name"]
-      ? row["Shipping Name"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping Name"]
-      : "";
-  };
-  const getShippingStreet = (row, previousData) => {
-    return row["Shipping Street"]
-      ? row["Shipping Street"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping Street"]
-      : "";
-  };
-  const getShippingCity = (row, previousData) => {
-    return row["Shipping City"]
-      ? row["Shipping City"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping City"]
-      : "";
-  };
-  const getShippingZip = (row, previousData) => {
-    return row["Shipping Zip"]
-      ? row["Shipping Zip"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping Zip"]
-      : "";
-  };
-  const getShippingProvince = (row, previousData) => {
-    return row["Shipping Province"]
-      ? row["Shipping Province"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping Province"]
-      : "";
-  };
-  const getShippingCountry = (row, previousData) => {
-    return row["Shipping Country"]
-      ? row["Shipping Country"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping Country"]
-      : "";
-  };
-  const getShippingPhone = (row, previousData) => {
-    return row["Shipping Phone"]
-      ? row["Shipping Phone"]
-      : row["Name"] === previousData["Name"]
-      ? previousData["Shipping Phone"]
-      : "";
-  };
-  const getEmail = (row) => {
-    return row["Email"] || "";
+  const handleDataFromWoo = (dataFromB) => {
+    setConvertedData((prev) => [...prev, ...dataFromB]);
   };
 
-  // end process data
-  const convertTime = (time) => {
-    if (typeof time === "number") {
-      const jsDate = moment.utc((time - 25569) * 86400 * 1000);
-      const formattedDate = jsDate.format("M/D/YYYY HH:mm");
-      return formattedDate;
-    } else {
-      return time;
-    }
-  };
-
-  // Xử lý NOTES
-  const isTypeOrStyleLine = (line) =>
-    line.startsWith("Type") || line.startsWith("Style");
-
-  const isSizeLine = (line) =>
-    line.startsWith("Size") ||
-    line.startsWith("Men Size") ||
-    line.startsWith("Women Size") ||
-    line.startsWith("Kid Size");
-
-  const isPersonalizationLine = (line) =>
-    line.startsWith("Your Personalization (Optional):") ||
-    line.startsWith("Custom Name") ||
-    line.startsWith("Your Name");
-
-  const isComboLine = (line) => line.startsWith("Combo");
-
-  const parseLines = (note) => {
-    const lines = note.split(/\r?\n/);
-    return lines.filter((line) => line && !line.startsWith("_"));
-  };
-
-  const processCombo = (line, currentObject) => {
-    let parts = line.startsWith("Combo::") ? line.split("::") : line.split(":");
-    let addOnProduct = parts[1].trim().startsWith("Jersey")
-      ? parts[1].replace("Jersey", "")
-      : parts[1];
-
-    if (currentObject.Type) {
-      currentObject.Type += addOnProduct;
-    } else if (currentObject.Style) {
-      currentObject.Style += addOnProduct;
-    }
-  };
-
-  const processKeyValueLine = (line, currentObject) => {
-    let parts = line.includes("::") ? line.split("::") : line.split(":");
-    parts = parts.filter((item) => item.trim() !== "");
-    currentObject[parts[0].trim()] = parts[1].trim();
-  };
-
-  const handleProcessNotes = (previousData) => {
-    let note = previousData["Notes"];
-    if (!note) return [];
-
-    const lines = parseLines(note);
-    const result = [];
-    let currentObject = {};
-
-    lines.forEach((line) => {
-      if (isTypeOrStyleLine(line)) {
-        if (currentObject["Personal"]) {
-          currentObject = {};
-        }
-        if (Object.keys(currentObject).length > 0) {
-          result.push(currentObject);
-        }
-        currentObject = {};
-      } else if (isSizeLine(line)) {
-      } else if (isComboLine(line)) {
-        processCombo(line, currentObject);
-        return;
-      } else if (isPersonalizationLine(line)) {
-        if (currentObject["Personal"]) {
-          currentObject = {};
-        }
-        line = line.split(":");
-        currentObject["Personal"] = line[1];
-
-        if (!currentObject["Type"] && !currentObject["Style"]) {
-          currentObject["Type"] = "";
-          currentObject["Size"] = "";
-        }
-        if (Object.keys(currentObject).length > 0) {
-          result.push(currentObject);
-        }
-        return;
-      } else {
-        currentObject["Personal"] =
-          (currentObject["Personal"] || "") + "\r\n" + line;
-        return;
-      }
-
-      processKeyValueLine(line, currentObject);
-    });
-
-    if (Object.keys(currentObject).length > 0) {
-      result.push(currentObject);
-    }
-    return result;
-  };
+  //set name
   const handleExport = () => {
     const ws = XLSX.utils.json_to_sheet(convertedData);
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, excelName);
-    XLSX.writeFile(wb, excelName + "_converted.xlsx");
+    XLSX.utils.book_append_sheet(wb, ws, shopifyName + "-" + wooName);
+    XLSX.writeFile(wb, shopifyName + "-" + wooName + "_converted.xlsx");
   };
   return (
-    <div className="p-4 space-y-4">
-      <h1 className="text-xl font-bold">Tycheco Excel Converter</h1>
-      <input
-        type="file"
-        accept=".xlsx, .xls, .csv"
-        onChange={handleFileUpload}
+    <div id="main-container">
+      <ShopifyConverter
+        onSendData={handleDataFromShopify}
+        setUploadName={setShopifyName}
       />
-
-      {convertedData.length > 0 && (
+      <WooConverter onSendData={handleDataFromWoo} setUploadName={setWooName} />
+      {convertedData.length > 0 && shopifyName && wooName && (
         <>
           <div>
             <button
@@ -275,7 +41,7 @@ export default function App() {
               Tải file đã xử lý
             </button>
           </div>
-          <table className="w-full table-auto border">
+          {/* <table className="w-full table-auto border">
             <thead>
               <tr>
                 <th className="border p-2">Created at</th>
@@ -326,7 +92,7 @@ export default function App() {
                 </tr>
               ))}
             </tbody>
-          </table>
+          </table> */}
         </>
       )}
     </div>
---
+++ src/components/wooConverter.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import moment from "moment";
+
+export default function ShopifyConverter({ onSendData, setUploadName }) {
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    setUploadName(file.name.replace(/\.(csv|xlsx)$/, ""));
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const data = new Uint8Array(evt.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+      // Chuyển đổi dữ liệu
+      const transformed = jsonData
+        .map((row) => {
+          return {
+            "Created at": row["( Order ) Create date"],
+            Name: row["( Order ) Order Number"],
+            "Lineitem name": row["( Order Item ) Product Name"],
+            "Lineitem quantity": row["( Order Item ) Item Quantity"],
+            "Lineitem sku": row["( Order Item ) Product Sku"],
+            "Shipping Name":
+              row["( Shipping ) First Name"] +
+              " " +
+              row["( Shipping ) Last Name"],
+            "Shipping Street": row["( Shipping ) Address 1"],
+            "Shipping Address2": row["( Shipping ) Address 2"], // Trống
+            "Shipping City": row["( Shipping ) City"],
+            "Shipping Zip": row["( Shipping ) Postcode"],
+            "Shipping Province": row["( Shipping ) State Code"],
+            "Shipping Country": row["( Shipping ) Country Name"],
+            "Shipping Country Code": row["( Shipping ) Country Code"], // Trống
+            "Shipping Phone": row["( Billing ) Phone"],
+            Email: row["( Billing ) Email"],
+            "Customer Notes": row["( Order ) Customer Note"], // Trống
+            Personalization: row["( Order Item )Your Text (Optional)"],
+            Type: getFirstStype(row["( Order Item )Style:"]),
+            Size: getFirstStype(row["( Order Item )Size:"]),
+            Notes: "",
+            "( Order Item ) Product Image Link":
+              row["( Order Item ) Product Image Link"],
+            "( Order Item ) Product Link": row["( Order Item ) Product Link"],
+            "( Order ) Payment Method Title":
+              row["( Order ) Payment Method Title"],
+            "( Order ) Total": row["( Order ) Total"],
+            "( Order ) Order Status": row["( Order ) Order Status"],
+          };
+        })
+        .filter((row) => Object.keys(row).length > 0); // Remove {}
+      onSendData(transformed);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+  const getFirstStype = (row) => {
+    if (row) {
+      row = row.split("|");
+      return row[0];
+    } else {
+      return "";
+    }
+  };
+  return (
+    <div className="p-4 space-y-4 uploader">
+      <h1 className="text-xl font-bold">Tải file Woo</h1>
+      <input
+        type="file"
+        accept=".xlsx, .xls, .csv"
+        onChange={handleFileUpload}
+      />
+    </div>
+  );
+}
